4fca70f3bab67bbee43f88eeeecf506251c8fe81
yuzheng14 <422450117@qq.com>
2021-03-12 20:42:46 +0800

    修改日志的编码问题

--- log.py
+++ log.py
@@ -1,5 +1,5 @@
 import time
 
 def log(message):
-    with open('log.out','a') as f:
+    with open('log.out','a',encoding='utf-8') as f:
         f.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'\t'+message+'\n')
